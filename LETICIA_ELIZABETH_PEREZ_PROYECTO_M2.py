03171ebffe6fb3dda206d4a0e098a9d64c0b80dc
lisiarmeli <lisiarmeli@gmail.com>
2024-02-11 20:06:34 -0600

    PROYECTO M2
    - len of a word

--- LETICIA_ELIZABETH_PEREZ_PROYECTO_M2.py
+++ LETICIA_ELIZABETH_PEREZ_PROYECTO_M2.py
@@ -1,1 +1,11 @@
+def longitud_palabra():
+    palabra = input("Ingrese una palabra: ")
 
+    if len(palabra) < 4 :
+        print("Hacen falta letras en la palabra")
+    elif len(palabra) >= 4 and len(palabra) <= 8 :
+        print("La palabra es correcta")
+    elif len(palabra) > 8 :
+        print("La palabra tiene " + str(len(palabra)) + " letras")
+
+longitud_palabra()
